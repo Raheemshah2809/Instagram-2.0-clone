b74e720f3fbd7297dbe9cbdb41b5edb83f8d2031
Raheemshah2809 <raheemshah2809@gmail.com>
2021-10-24 16:04:03 +0100

    v0.1.1

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  
+
+        {/*header*/}
+          <Header />
+        {/*Feed*/}
+        
+        {/*Modal*/}
+
+
+    </div>
+  )
+}
