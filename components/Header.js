bb9a21bc0a87ac3e4eced74f706bb50a8a2812e6
Raheemshah2809 <raheemshah2809@gmail.com>
2021-10-24 19:26:01 +0100

    v0.1.2

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,29 @@
 import Image from "next/image";
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon,
+} from "@heroicons/react/outline";
+import {HomeIcon} from "@heroicons/react/solid";
+
 
 
 function Header() {
     return (
         <div>
-            <div className="flex justify-between max-w-6xl">
+            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
                 {/*left*/}
 
-                <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+                <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
                     <Image src="https://links.papareact.com/ocw" layout="fill" 
                     objectFit="contain"/>
 
                 </div>
 
-                <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+                <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer hover:scale-125 transition-all duration-150 ease-out">
                     <Image 
                     src="https://links.papareact.com/jjm"
                     layout="fill"
@@ -21,14 +31,31 @@ function Header() {
                 </div>
 
                 {/*Middle*/}
-
-                <div>
-                        <input type="text" placeholder="Search"/>
-
-
+                    <div className="max-w-xs">
+                <div className="relative mt-1 p-3 rounded-md">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none ">
+                        <SearchIcon className="h-5 w-5 text-gray-500"/>
+                    </div>
+                        <input className="bg-gray-50 block w-full pl-10 sm:text-sm  focus:ring-black focus:border-black rounded-md" type="text" placeholder="Search"/>
                 </div>
+                    </div>
 
                 {/*Right*/}
+                <div className="flex items-center justify-end space-x-4">
+                <HomeIcon className="navBtn"/>
+                <MenuIcon className="h-6 md:hidden cursor-pointer"/>
+                <div className="relative navBtn">
+                <PaperAirplaneIcon className="navBtn rotate-45"/>
+                <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                3
+                </div>
+                </div>
+                <PlusCircleIcon className="navBtn" />
+                <UserGroupIcon className="navBtn"/>
+                <HeartIcon className="navBtn"/>
+
+                <img src= "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg" alt= "the profile picture" className="h-10 rounded-full cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+                </div>
             </div>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'tailwindcss/tailwind.css'
+import "/Users/raheemshah/Documents/Github Projects/Instagram-2.0-clone/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
