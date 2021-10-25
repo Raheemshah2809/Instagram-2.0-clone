72f699ef9d6992e9de8ef1ec5c1993c8ed5c5a42
Raheemshah2809 <raheemshah2809@gmail.com>
2021-10-26 00:14:24 +0100

    v0.1.51

--- components/Header.js
+++ components/Header.js
@@ -8,10 +8,14 @@ import {
     MenuIcon,
 } from "@heroicons/react/outline";
 import {HomeIcon} from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 
 
 function Header() {
+    const {data: session} = useSession();
+
+    console.log(session);
     return (
         <div className="shadow-sm border-b bg-white sticky top-0 z-50">
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -44,17 +48,28 @@ function Header() {
                 <div className="flex items-center justify-end space-x-4">
                 <HomeIcon className="navBtn"/>
                 <MenuIcon className="h-6 md:hidden cursor-pointer"/>
-                <div className="relative navBtn">
-                <PaperAirplaneIcon className="navBtn rotate-45"/>
-                <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center text-white">
-                89
-                </div>
-                </div>
-                <PlusCircleIcon className="navBtn" />
-                <UserGroupIcon className="navBtn"/>
-                <HeartIcon className="navBtn"/>
-
-                <img src= "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg" alt= "the profile picture" className="h-10 rounded-full cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+                {session ? (
+                    <>
+                    <div className="relative navBtn">
+                    <PaperAirplaneIcon className="navBtn rotate-45"/>
+                    <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center text-white">
+                    89
+                    </div>
+                    </div>
+                    <PlusCircleIcon className="navBtn" />
+                    <UserGroupIcon className="navBtn"/>
+                    <HeartIcon className="navBtn"/>
+    
+                    <img 
+                    onClick={signOut}
+                    src={session?.user?.image} 
+                    alt= "the profile picture" className="h-10 rounded-full cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+                    </>
+                    
+                ):(
+                    <button onClick={signIn}>Sign In</button>
+                )}
+                
                 </div>
             </div>
         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "/Users/raheemshah/Documents/Github Projects/Instagram-2.0-clone/styles/globals.css";
+import {SessionProvider} from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+  return(
+
+    <SessionProvider session={session}>
+    
+    <Component {...pageProps} />
+    </SessionProvider>
+  )
+  
+  
 }
 
 export default MyApp
