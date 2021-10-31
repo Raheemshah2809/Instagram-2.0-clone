3b2b5cec6597ff428a18e7660a73d05e95df0a62
Raheemshah2809 <raheemshah2809@gmail.com>
2021-10-31 19:43:14 +0000

    Name Change

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
-        <title>Instagram</title>
+        <title>Marketplace</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
           <Header />
