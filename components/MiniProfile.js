e6bde25ca6b060c968cef027798448fa1e945e9c
Raheem Shah <66431658+Raheemshah2809@users.noreply.github.com>
2021-12-01 12:10:21 +0000

    Minor Change 1

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -10,7 +10,7 @@ function MiniProfile() {
 
         <div className="flex-1 mx-4">
             <h2 className="font-bold">{session?.user?.username}</h2>
-            <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+            <h3 className="text-sm text-gray-400"></h3>
         </div>
 
 <button onClick={() => {
