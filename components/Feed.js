f11c17ad046742aa67b67d335ff395e4e082e360
Raheem Shah <66431658+Raheemshah2809@users.noreply.github.com>
2021-12-01 12:24:58 +0000

    Test 3 (Final)

--- components/Feed.js
+++ components/Feed.js
@@ -10,7 +10,6 @@ function Feed() {
         <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
             <section className="col-span-2"> 
             {session && (
-                <Stories />
 
             )}
             <Posts />
@@ -19,8 +18,8 @@ function Feed() {
 
                             <section className="hidden xl:inline-grid md:col-span-1">
                                 <div className="fixed top-20">
-                
-
+                    
+                                
                 
                                 </div>
                             </section>
